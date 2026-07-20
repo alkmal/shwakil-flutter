b387f9c4cb2a265d711157cc955715df1d47b9ec
Kamal Alomari <kmal.alomari@gmail.com>
2026-07-20 21:15:27 +0300

    chore(android): brand maintenance trial build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,7 +63,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.alkmal.shwakil"
+        // Keep the maintenance trial installable alongside the public Shwakil app.
+        applicationId = "com.alkmal.shwakil.maintenance_trial"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -90,6 +91,7 @@ android {
         }
         release {
             signingConfig = signingConfigs.getByName("release")
+            versionNameSuffix = "-maintenance-trial"
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
